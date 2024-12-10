153b96ff6ed359f609c7a4edb1fcfdb9ecb992d5
Alberto Monaco <alberto.monaco84@gmail.com>
2024-12-10 12:57:24 +0100

    fix index

--- controllers/MoviesController.js
+++ controllers/MoviesController.js
@@ -1,7 +1,8 @@
 const connection = require('../database/connection')
 
 function index(req, res) {
-	connection.query(`SELECT * FROM movies`, (err, results) => {
+	const sql = `SELECT * FROM movies`
+	connection.query(sql, (err, results) => {
 		if (err) return res.status(500).json({ err: err })
 
 		res.json({
