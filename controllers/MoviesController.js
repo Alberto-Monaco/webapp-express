6ceab3bf0ac07026ab01c8cc1209c5b4c62624f2
Alberto Monaco <alberto.monaco84@gmail.com>
2024-12-10 12:52:17 +0100

    add middleware not found and server error

--- controllers/MoviesController.js
+++ controllers/MoviesController.js
@@ -15,21 +15,15 @@ function show(req, res) {
 	const id = req.params.id
 	const sql = `SELECT * FROM movies WHERE id = ?`
 
-	// get all reviews for a book
 	const reviewsSql = `SELECT * FROM reviews WHERE movie_id=?`
 
-	// get the book by the given id
 	connection.query(sql, [id], (err, results) => {
 		if (err) return res.status(500).json({ err: err })
 		if (results.length == 0) return res.status(404).json({ err: 'movie not found' })
 
-		//console.log(results);
-
-		// get all reviews associated to the book
 		connection.query(reviewsSql, [id], (err, reviewsResults) => {
 			if (err) return res.status(500).json({ err: err })
 
-			//console.log('reviews', reviewsResults);
 			const movie = {
 				...results[0],
 				reviews: reviewsResults
