05f44e22708676bcd25b649747abe57cbc07b241
Alberto Monaco <alberto.monaco84@gmail.com>
2024-12-10 12:47:39 +0100

    import route in app

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const server = express()
-const connection = require('./database/connection')
+
+const MoviesRoutes = require('./routes/MoviesRoutes')
+
 const port = process.env.PORT
 const host = process.env.HOST
-server.use(express.json())
 
+server.get('/', (req, res) => {
+	res.send(`Server is up and running!`)
+})
+
+server.use('/films', MoviesRoutes)
 server.listen(port, () => {
 	console.log(`Server is running on ${host}:${port}`)
 })
