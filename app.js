c53ba005cd4d7c75a7e73c425f73794c0d24efea
Alberto Monaco <alberto.monaco84@gmail.com>
2024-12-10 12:55:39 +0100

    add in app middleware

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const server = express()
 
+const NotFound = require('./middleware/notFound')
+const ServerErrorsHandler = require('./middleware/ServerErrorsHandler')
+
 const MoviesRoutes = require('./routes/MoviesRoutes')
 
 const port = process.env.PORT
@@ -10,7 +13,11 @@ server.get('/', (req, res) => {
 	res.send(`Server is up and running!`)
 })
 
-server.use('/films', MoviesRoutes)
+server.use('/movies', MoviesRoutes)
+
+server.use(NotFound)
+server.use(ServerErrorsHandler)
+
 server.listen(port, () => {
 	console.log(`Server is running on ${host}:${port}`)
 })
