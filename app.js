3f992349728fe2d75bbea31b94ae517c786e4426
Alberto Monaco <alberto.monaco84@gmail.com>
2024-12-10 12:23:52 +0100

    create app and listen server

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const server = express()
+const port = process.env.PORT
+const host = process.env.HOST
+server.use(express.json())
+
+server.listen(port, () => {
+	console.log(`Server is running on ${host}:${port}`)
+})
